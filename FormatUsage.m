a5b534a3da54f46a5511fa3a497dd98fa044a240
McSaks <mcsaksik@gmail.com>
2015-07-10 18:41:08 +0300

    A rule for _var\_i_ for subscript added.

--- FormatUsage.m
+++ FormatUsage.m
@@ -5,6 +5,10 @@ FormatUsage[str_] := StringReplace[str,
   "`" ~~ Shortest[s__] ~~ "`" :>
   "\!\(\*StyleBox[\(" <> StringReplace[s, {
     RegularExpression["(?!</)_"] ~~
+      Shortest[i__] ~~ "\_" ~~ Shortest[num__] ~~
+      RegularExpression["(?!</)_"] :>
+        "\*StyleBox[\(" <> i <> "\), \"TI\"]\_\*StyleBox[\(" <> num <> "\), \"TR\", \"TraditionalForm\"]",
+    RegularExpression["(?!</)_"] ~~
       Shortest[i__] ~~
       RegularExpression["(?!</)_"] :>
         "\*StyleBox[\(" <> i <> "\), \"TI\"]",
